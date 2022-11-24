6ea128c5259f50c745ce1e1c05b6233937ee8198
Muhammad Jafar Ash Siddiq <mjafarashsiddiq@gmail.com>
2022-11-24 14:50:21 +0700

    finish first task make home travel page

--- TravelTaskWeek2/ContentView.swift
+++ TravelTaskWeek2/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("go-to-beach")
+                .resizable()
+                .ignoresSafeArea()
+                .overlay{
+                    Color(.black)
+                        .ignoresSafeArea()
+                        .opacity(0.4)
+                }
+            VStack {
+                HStack {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .font(.system(size: 25))
+                        .foregroundColor(.white)
+                        .accentColor(.yellow)
+                        .padding(15)
+                        .background{
+                            Circle()
+                                .opacity(0.2)
+                        }
+                    Spacer()
+                    
+                    Text("Travel")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    
+                    Spacer()
+                    Image(systemName: "cart.badge.plus")
+                        .renderingMode(.original)
+                        .font(.system(size: 25))
+                        .foregroundColor(.white)
+                        .accentColor(.yellow)
+                        .padding(15)
+                        .background{
+                            Circle()
+                                .opacity(0.2)
+                        }
+                    
+                }
+                VStack(alignment: .leading) {
+                    Text("Start your Adventure and Go Travel Now")
+                        .foregroundColor(.white)
+                        .font(.system(size: 45,design: .rounded))
+                        .bold()
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 70))
+                    Text("Discover local destinations beauty with Bluesky and travel with pride")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Spacer()
+                    
+                }
+                
+            }// Vstack
+            .padding(30)
+            
         }
-        .padding()
     }
 }
 
