ee78e817b5b54e13e41a0a8fc1b5350c0b91b182
Muhammad Jafar Ash Siddiq <mjafarashsiddiq@gmail.com>
2022-11-24 14:16:20 +0700

    Initial Commit

---
+++ TravelTaskWeek2/TravelTaskWeek2App.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTaskWeek2App.swift
+//  TravelTaskWeek2
+//
+//  Created by Muhammad Jafar Ash Siddiq on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelTaskWeek2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
